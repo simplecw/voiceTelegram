a49915b12513415098ed7025c59c4abf9b5cfaad
wei chen <simplecw@gmail.com>
2025-06-24 17:58:12 +0800

    something update

--- bot.py
+++ bot.py
@@ -1,16 +1,6 @@
 from flask import Flask
-from main import main as main_func
-from google_drive_tools import main as drive_func
 
 app = Flask(__name__)
-
-@app.route("/run-main", methods=["GET"])
-def call_main():
-    return main_func()
-
-@app.route("/run-drive", methods=["GET"])
-def call_drive():
-    return drive_func()
 
 @app.route("/", methods=["GET"])
 def home():
