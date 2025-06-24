dc31df932e7b9e9da6131d8e64db75a4ff04ea6a
simplecw <simplecw@gmail.com>
2025-06-24 20:49:30 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from flask import Flask
-# from google_drive_tools import main
+from google_drive_tools import main
 
 app = Flask(__name__)
 
 @app.route("/run-drive", methods=["GET"])
 def call_drive():
-    # result = google_drive_tools.main()
-    # return result if result else "No output from drive_func"
-    return "No output from drive_func"
+    result = google_drive_tools.main()
+    return result if result else "No output from drive_func"
+    # return "No output from drive_func"
 
 @app.route("/", methods=["GET"])
 def home():
